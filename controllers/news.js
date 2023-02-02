7c89d36e4a5ad67295be90f6b3f82b1d73f04944
mar69287 <marcoa.ruiz97@gmail.com>
2023-02-01 22:36:51 -0800

    media queries

--- controllers/news.js
+++ controllers/news.js
@@ -4,10 +4,11 @@ module.exports = {
 };
 
 function index(req, res, next) {
-    fetch("https://newsapi.org/v2/everything?q=Programming&from=2023-01-01&sortBy=popularity&apiKey=e57c19337c124e52a59bc9d832327d12")
+    fetch("https://newsapi.org/v2/everything?q=Programming&from=2023-02-01&sortBy=popularity&apiKey=e57c19337c124e52a59bc9d832327d12")
         .then(res => res.json())
         .then(newsInfo =>
             // console.log(info.articles[0].author)
+            // console.log(newsInfo))
             res.render('news/index', { title: 'Code Universe', newsInfo }));
 }
 
